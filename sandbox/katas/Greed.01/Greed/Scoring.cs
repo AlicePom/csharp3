250755737201e3588a94fc16d21134b7dffea764
AlicePom <129623472+AlicePom@users.noreply.github.com>
2024-09-29 15:53:45 +0200

    Scoring method simplified

--- sandbox/katas/Greed.01/Greed/Scoring.cs
+++ sandbox/katas/Greed.01/Greed/Scoring.cs
@@ -14,29 +14,26 @@ public class Scoring
     }
     public void EvaluateScore(Player player) // Scoring the game
     {
-        // Scoring for a Straight - 1, 2, 3, 4, 5, 6
-        player.AllThrows.Sort(); // sorting the thrown numbers
-        List<int> straight = new List<int>() { 1, 2, 3, 4, 5, 6 };
+        var groupedThrows = player.AllThrows.GroupBy(n => n) // grouping of numbers in the set
+                            .Select(g => new { Number = g.Key, Count = g.Count() })
+                            .ToList();
 
-        if (player.AllThrows.SequenceEqual(straight))
+        // Scoring for Straight
+        if (groupedThrows.Count == 6) // if there are 6 groups (unique numbers)
         {
             Score += 1200;
             Console.WriteLine("You scored a Straight!");
             return; // Terminates the method since no other scoring evaluation is necessary
         }
 
-        // Scoring of Three Pairs
-        var groupedThrows = player.AllThrows.GroupBy(n => n) // grouping of numbers in the set
-                            .Select(g => new { Number = g.Key, Count = g.Count() })
-                            .ToList();
-
+        // Scoring for Three Pairs
         if (groupedThrows.Count == 3) // if there are 3 distinct numbers in the set
         {
-            int pair = 0;
+            int pair = 0; //the number of all pairs
 
             foreach (var group in groupedThrows)
             {
-                if (group.Count == 2) // if the count of each group makes a pare
+                if (group.Count == 2) // if the count of each group makes a pair
                 {
                     pair += 1;
                 }
@@ -51,46 +48,28 @@ public class Scoring
         }
 
         // Scoring for Three- or More-of-a-Kind & for remaining individual 1s and 5s
-        for (int i = 1; i <= 6; i++) // evaluation of count of individual number from 1 to 6
+        foreach (var group in groupedThrows)
         {
-            // Counting the number of i
-            int countI = player.AllThrows.Count(n => n == i);
-
-            // A koeficient is introduced for i == 1 to be evaluated by 10x more points
-            int koeficientI = 1;
-            if (i == 1)
+            int koeficient = 1; // A koeficient is introduced for number 1 to be evaluated by 10x more points
+            if (group.Number == 1)
             {
-                koeficientI *= 10;
+                koeficient *= 10;
             }
 
-            // Scoring
-            int extraCount = countI - 3; // extraCount is a utility number that expresses extra multiples (More-of-a-Kind) above 3
-            if (extraCount >= 0)
+            if (group.Count >= 3) // Scoring for Three- or More-of-a-Kind
             {
-                Score += (100 * i * koeficientI) * Math.Pow(2, extraCount);
-                Console.WriteLine($"You scored a {countI}-of-a-Kind made of {i}!");
+                //int extraCount = group.Count - 3; // to be removed
+                Score += (100 * group.Number * koeficient) * Math.Pow(2, (group.Count - 3));
+                Console.WriteLine($"You scored a {group.Count}-of-a-Kind made of {group.Number}!");
+            }
 
-                // removing the numbers counted for scoring from the allThrows list for further possible scoring (e.g., other triples,
-                for (int j = 1; j <= countI; j++)
+            if (group.Count < 3) // Scoring for remaining individual 1s and 5s
+            {
+                if (group.Number == 1 || group.Number == 5)
                 {
-                    player.AllThrows.Remove(i);
+                    Score += group.Number * 10 * koeficient * group.Count;
+                    Console.WriteLine($"You scored {group.Count}x number {group.Number}.");
                 }
-
-                // re-counting individual number i count after removal of the possible matches above
-                countI = player.AllThrows.Count(n => n == i);
-            }
-
-            // Scoring each individual 1 or 5 that remained in the set after all previous Three- or More-of-a-Kind scorings
-            if (i == 1 && countI > 0)
-            {
-                Score += 100 * countI;
-                Console.WriteLine($"You scored {countI}x number 1.");
-            }
-
-            if (i == 5 && countI > 0)
-            {
-                Score += 50 * countI;
-                Console.WriteLine($"You scored {countI}x number 5.");
             }
         }
     }
